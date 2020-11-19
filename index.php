9caee0b88c5e6f28e87d603a42947ffa9fef0862
salahalmahady <74627231+salahalmahady@users.noreply.github.com>
2020-11-19 15:57:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+    <title>
+    open source software midterm
+    </title>
+    </title></head>
+    <pody>
+    <?php
+    
+    $ txt1 ="كلية الامارات للعلوم و التكنولوجيا";
+    $ txt2 ="اسم الطالب :";
+    $ z ="رقم الطالب :";
+    $ txt3 ="قسم الطالب :";
+    $ txt4 ="الفصل الدراسي :";
+    $ txt5 ="المستوي الدراسي :";
+    echo '$ txt1'. ;
+    echo $txt2."salah almahady mohamed";
+    echo $z."310480190";
+    echo $txt3."it";
+    echo $txt4."الخامس ";
+    echo $txt5."الثالث";
+    ?>
+    </body>
+    </html>
